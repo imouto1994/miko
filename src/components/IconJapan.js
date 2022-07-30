f6bd52efda4e34bf68a90ab12e169c853563fec9
Nhan Bui <buidev94@gmail.com>
2022-07-31 05:17:53 +0800

    Pass className to IconJapan

--- src/components/IconJapan.js
+++ src/components/IconJapan.js
@@ -4,7 +4,11 @@ export default function IconJapan(props) {
   const { className } = props;
 
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      className={className}
+    >
       <path
         fill="#f5f5f5"
         d="M473.65 88.28H38.35A38.34 38.34 0 0 0 0 126.62v258.76a38.34 38.34 0 0 0 38.34 38.34h435.31A38.34 38.34 0 0 0 512 385.38V126.62a38.34 38.34 0 0 0-38.35-38.34z"
