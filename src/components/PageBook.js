ae39501d11bed7ab2448b650c7a74c36f2bb552b
Nhan Bui <buidev94@gmail.com>
2022-07-31 21:12:28 +0800

    Fix reading direction not updated properly

--- src/components/PageBook.js
+++ src/components/PageBook.js
@@ -122,7 +122,7 @@ export default function PageBook(props) {
         }
       }
     },
-    [goBack, goForward, setNavHidden]
+    [goBack, goForward, setNavHidden, direction]
   );
 
   const onReadingModeButtonClick = useCallback(() => {
